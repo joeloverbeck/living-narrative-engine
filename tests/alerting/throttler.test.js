ae954b2820f6ea7cff7934895c23c17ee6f67fd9
joeloverbeck <joeloverbeck@gmail.com>
2025-06-07 10:45:53 +0200

    Remove unused exitResolver

--- tests/alerting/throttler.test.js
+++ tests/alerting/throttler.test.js
@@ -1,6 +1,6 @@
 /**
  * @file Unit tests for the Throttler class.
- * @see ../../src/alerting/Throttler.js
+ * @see ../../src/alerting/throttler.js
  */
 
 import {
@@ -11,7 +11,7 @@ import {
   afterEach,
   jest,
 } from '@jest/globals';
-import { Throttler } from '../../src/alerting/Throttler.js';
+import { Throttler } from '../../src/alerting/throttler.js';
 
 // Create a mock dispatcher that conforms to the ISafeEventDispatcher interface for testing.
 const mockDispatcher = {
