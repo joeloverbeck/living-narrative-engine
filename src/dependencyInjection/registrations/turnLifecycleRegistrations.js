f9eca44367629a68d82a24d45bbdddedf3be61c5
joeloverbeck <joeloverbeck@gmail.com>
2025-06-14 15:48:32 +0200

    Uploading with errors for Codex to fix.

--- src/dependencyInjection/registrations/turnLifecycleRegistrations.js
+++ src/dependencyInjection/registrations/turnLifecycleRegistrations.js
@@ -26,7 +26,7 @@ import { TurnActionChoicePipeline } from '../../turns/pipeline/turnActionChoiceP
 import { HumanDecisionProvider } from '../../turns/providers/humanDecisionProvider';
 import { GenericTurnStrategyFactory } from '../../turns/factories/genericTurnStrategyFactory.js';
 import { TurnContextBuilder } from '../../turns/builders/turnContextBuilder.js';
-import { assertValidActor } from '../../utils/validation/actorValidation.js';
+import { assertValidActor } from '../../utils/actorValidation.js';
 
 /**
  * @param {import('../appContainer.js').default} container
