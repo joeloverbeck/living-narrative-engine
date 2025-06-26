e3e1d50e1b849533c46f90e3781fc285fc25923d
joeloverbeck <joeloverbeck@gmail.com>
2025-06-26 17:43:55 +0200

    [Agent] add specific validation errors

--- src/errors/index.js
+++ src/errors/index.js
@@ -4,3 +4,5 @@ export * from './invalidInstanceIdError.js';
 // ... add other exports as needed
 export * from './invalidEntityIdError.js';
 export * from './InitializationError.js';
+export * from './invalidActionDefinitionError.js';
+export * from './invalidActorEntityError.js';
