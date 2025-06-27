e92f39b77dfcb9313835d90fd202f4ecf628e2bd
joeloverbeck <joeloverbeck@gmail.com>
2025-06-27 21:38:00 +0200

    [Agent] clarify entity manager adapter interface

--- src/entities/entityManagerAdapter.js
+++ src/entities/entityManagerAdapter.js
@@ -1,46 +1,120 @@
 import { IEntityManager } from '../interfaces/IEntityManager.js';
 
 /**
- * @description Adapter that delegates to the underlying EntityManager while providing location query methods.
+ * Explicit adapter around {@link import('./entityManager.js').default | EntityManager}.
+ * Only exposes a curated subset of the manager API plus location queries.
+ * This avoids leaking unintended methods to consumers via a Proxy.
+ *
  * @class EntityManagerAdapter
  * @implements {IEntityManager}
  */
 export class EntityManagerAdapter extends IEntityManager {
+  /** @type {import('./entityManager.js').default} */
+  #entityManager;
+  /** @type {import('./locationQueryService.js').LocationQueryService} */
+  #locationQueryService;
+
   /**
+   * Create a new adapter instance.
+   *
    * @param {object} dependencies - Constructor dependencies.
-   * @param {import('./entityManager.js').default} dependencies.entityManager - Concrete EntityManager instance.
-   * @param {import('./locationQueryService.js').LocationQueryService} dependencies.locationQueryService - Service used for location queries.
+   * @param {import('./entityManager.js').default} dependencies.entityManager - Wrapped entity manager.
+   * @param {import('./locationQueryService.js').LocationQueryService} dependencies.locationQueryService - Service used for spatial queries.
    */
   constructor({ entityManager, locationQueryService }) {
     super();
-    this.entityManager = entityManager;
-    this.locationQueryService = locationQueryService;
+    this.#entityManager = entityManager;
+    this.#locationQueryService = locationQueryService;
+  }
 
-    return new Proxy(this, {
-      get(target, prop, receiver) {
-        if (
-          Object.hasOwn(target, prop) ||
-          Object.hasOwn(EntityManagerAdapter.prototype, prop)
-        ) {
-          return Reflect.get(target, prop, receiver);
-        }
-        const value = entityManager[prop];
-        if (typeof value === 'function') {
-          return value.bind(entityManager);
-        }
-        return value;
-      },
-    });
+  // ----------------------- IEntityManager delegation -----------------------
+  /** @inheritdoc */
+  getEntityInstance(id) {
+    return this.#entityManager.getEntityInstance(id);
+  }
+
+  /** @inheritdoc */
+  createEntityInstance(definitionId, options = {}) {
+    return this.#entityManager.createEntityInstance(definitionId, options);
+  }
+
+  /** @inheritdoc */
+  reconstructEntity(serializedEntity) {
+    return this.#entityManager.reconstructEntity(serializedEntity);
+  }
+
+  /** @inheritdoc */
+  getComponentData(instanceId, componentTypeId) {
+    return this.#entityManager.getComponentData(instanceId, componentTypeId);
+  }
+
+  /** @inheritdoc */
+  hasComponent(instanceId, componentTypeId) {
+    return this.#entityManager.hasComponent(instanceId, componentTypeId);
+  }
+
+  /** @inheritdoc */
+  hasComponentOverride(instanceId, componentTypeId) {
+    return this.#entityManager.hasComponentOverride(
+      instanceId,
+      componentTypeId
+    );
+  }
+
+  /** @inheritdoc */
+  getEntitiesWithComponent(componentTypeId) {
+    return this.#entityManager.getEntitiesWithComponent(componentTypeId);
+  }
+
+  /** @inheritdoc */
+  addComponent(instanceId, componentTypeId, componentData) {
+    return this.#entityManager.addComponent(
+      instanceId,
+      componentTypeId,
+      componentData
+    );
+  }
+
+  /** @inheritdoc */
+  removeComponent(instanceId, componentTypeId) {
+    return this.#entityManager.removeComponent(instanceId, componentTypeId);
+  }
+
+  /** @inheritdoc */
+  getEntitiesInLocation(id) {
+    return this.#locationQueryService.getEntitiesInLocation(id);
+  }
+
+  /** @inheritdoc */
+  getEntityIds() {
+    return this.#entityManager.getEntityIds();
+  }
+
+  /** @inheritdoc */
+  findEntities(query) {
+    return this.#entityManager.findEntities(query);
+  }
+
+  /** @inheritdoc */
+  get entities() {
+    return this.#entityManager.entities;
+  }
+
+  /** @inheritdoc */
+  getAllComponentTypesForEntity(entityId) {
+    return this.#entityManager.getAllComponentTypesForEntity(entityId);
   }
 
   /**
-   * Retrieves all entity instance IDs present in a specific location.
-   *
-   * @param {string} id - The unique location ID.
-   * @returns {Set<string>} Set of entity instance IDs in the location.
+   * Clears all active entities via the wrapped EntityManager.
+   * This method is not part of {@link IEntityManager} but required by
+   * subsystems like GameStateRestorer.
    */
-  getEntitiesInLocation(id) {
-    return this.locationQueryService.getEntitiesInLocation(id);
+  clearAll() {
+    if (typeof this.#entityManager.clearAll === 'function') {
+      return this.#entityManager.clearAll();
+    }
+    return undefined;
   }
 }
 
